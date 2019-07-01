1b14ee05c8ac7421207da04d6e578d231fe882a1
ghalib5000 <ghalibahmed5000@gmail.com>
2019-07-02 01:28:49 +0500

    done with the basic infrastructure

---
+++ NetRunRate/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace NetRunRate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StreamReader file;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        RateCalculator.Rate_Calculator manage = new RateCalculator.Rate_Calculator();
+        public int runscore = 0, rungive = 0;
+        public double overface = 0, overgive = 0;
+        public double res = 0;
+
+        private void Okbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (runsConceded.Text != "" && runsScored.Text != "" && oversBowled.Text != "" && oversFaced.Text != "")
+            {
+                try
+                {
+                   rungive = Convert.ToInt32(runsConceded.Text);
+                    runscore = Convert.ToInt32(runsScored.Text);
+                    overface = Convert.ToDouble(oversFaced.Text);
+                    overgive = Convert.ToDouble(oversBowled.Text);
+                    res = manage.calculate(runscore, rungive, overface, overgive);
+                    Result.Text = Convert.ToString(res);
+                }
+                catch (Exception ex)
+                {
+                    Result.Text = ex.Message;
+                }
+
+            }
+            //reading the data from file
+            else if (dataFile.Text != "")
+            {
+                try
+                {//Pass the file path and file name to the StreamReader constructor
+                    file = new StreamReader(dataFile.Text);
+
+                    runscore = Convert.ToInt32(file.ReadLine());
+                    overface = Convert.ToDouble(file.ReadLine());
+                    rungive = Convert.ToInt32(file.ReadLine());
+                    overgive = Convert.ToDouble(file.ReadLine());
+                   res =  manage.calculate(runscore, rungive, overface, overgive);
+                    Result.Text = Convert.ToString(res);
+                    file.Close();
+                }
+                 catch (Exception ex)
+                   {
+                    Result.Text = ex.Message;
+                   }
+            }
+            else
+            {
+                Result.Text = " please input correct numbers";
+            }
+        }
+    }
+}
---
+++ NetRunRate/RateCalculator/Rate_Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetRunRate.RateCalculator
+{
+    class Rate_Calculator
+    {
+        
+        private double num1 ,num2;
+        string[] numberstorer = new string[2];
+        private string temp;
+        public double calculate(int runscore,int rungive, double overface, double overgive)
+        {
+            double output=0.0;
+          overface =  pointchecker(overface);
+            overgive = pointchecker(overgive);
+            num1 = (runscore / overface);
+            num2 = (rungive / overgive);
+            output = num1 - num2;
+
+            return output;
+        }
+        private double pointchecker(double num)
+        { temp = Convert.ToString(num);
+
+            if(temp.Contains('.'))
+            {
+                double i=0;
+                string temp1, temp2;
+               numberstorer= temp.Split('.');
+                temp1 = numberstorer[0];
+                temp2 = numberstorer[1];
+                i = Convert.ToDouble(temp2);
+                temp2 = Convert.ToString((i / 6));
+                numberstorer = temp2.Split('.');
+                temp2 = '.'+ numberstorer[1];
+                temp = temp1 + temp2;
+                num = Convert.ToDouble(temp);
+            }
+            return num;
+           
+        }
+    }
+}
